64b07e4d5f481448727c322ef42f4a38a6714752
Bibby <bibby.yeh@frog-jump.com>
2023-04-24 17:56:28 +0800

    todo lab

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,16 +1,33 @@
 import React, { Component } from 'react'
 import './index.css'
 export default class index extends Component {
+
+  clearComletedTask = () => {
+    this.props.clearComletedTask()
+  }
+
+  updateTodoCheck = (event) => {
+    this.props.updateTodoCheck(event.target.checked)
+  }
+
   render() {
+    const {todos} = this.props
+    const doneCount = todos.reduce((pre,curr)=>{
+        return pre + (curr.done ? 1 : 0);
+    },0)
+    const total = todos.length
+    // const doneCount = todos.reduce((pre,todo)=> pre + (todo.done ? 1 : 0),0)  
+    console.log('doneCount',doneCount)
+    console.log('total',total)
     return (
       <div className="todo-footer">
           <label>
-              <input type="checkbox"/>
+              <input type="checkbox" onChange={this.updateTodoCheck} checked = {doneCount === total ? ( doneCount!==0 ?true : false) : false}/>
           </label>
           <span>
-              <span>已完成0</span> / 全部2
+              <span>已完成{doneCount}</span> / 全部{total}
           </span>
-          <button className="btn btn-danger">清除已完成任務</button>
+          <button className="btn btn-danger" onClick={this.clearComletedTask}>清除已完成任務</button>
       </div>
     )
   }
--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
+import './index.css'
 
 export default class index extends Component {
+
+  state = {mouse:false} //標識鼠標移入、移出
+
+  handleMouse = (flag) => {
+    return () => {
+      this.setState({mouse:flag});
+    }
+  }
+
+  handleCheck = (id) => {
+    return (event) => {
+      this.props.updateTodo(this.props.id,event.target.checked)
+    }
+  }
+
+  deleteTodoItem = (id) => {  
+    if(window.confirm('確定刪除嗎？')) {
+      this.props.deleteTodoItem(id);
+    }
+  }
+
   render() {
+    const {id,name,done} = this.props;
+    const {mouse} = this.state;
     return (
-      <>
+ 
+      <li style={{backgroundColor:mouse ? '#ddd' : 'white'}} 
+        onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)}>
         
-      </>
+        <label htmlFor={id}><input type='checkbox' id={id} checked={done} onChange={this.handleCheck(id)}/>
+        {name}</label>
+        <button className='btn btn-danger' style={{display:mouse ? 'block' : 'none'}} onClick={() => {this.deleteTodoItem(id)}}>刪除</button>
+      </li>
+
     )
   }
 }
