32756d3b115b6ce52c5b89993c564f6a3e1fe94a
Bibby <bibby.yeh@frog-jump.com>
2023-04-25 11:24:21 +0800

    to init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+import Hello from './components/Hello'
+import Welcome from './components/Welcome'
+import './App.css'
+
+export default class App extends Component{
+	render(){
+		return (
+			<div>
+			</div>
+		)
+	}
+}
